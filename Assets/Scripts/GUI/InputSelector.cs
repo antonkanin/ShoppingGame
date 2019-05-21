66ca36b26c8fc0340323383a5869ef9b6cbadd3a
antonkanin <antony.kanin@gmail.com>
2019-05-21 15:17:55 +0300

    adding input selector - work in progress

---
+++ Assets/Scripts/GUI/InputSelector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputSelector : MonoBehaviour
+{
+    public Toggle twoJoysticksToggle;
+    public Toggle oneJoysticksToggle;
+    public Toggle buttonsToggle;
+
+    private void Start()
+    {
+        twoJoysticksToggle.isOn = true;
+        oneJoysticksToggle.isOn = false;
+        buttonsToggle.isOn = false;
+    }
+
+    public void SelectToggle(Toggle toggle)
+    {
+/*
+        twoJoysticksToggle.isOn = twoJoysticksToggle.isOn != twoJoysticksToggle;
+        oneJoysticksToggle.isOn = oneJoysticksToggle.isOn != twoJoysticksToggle;
+        buttonsToggle.isOn = buttonsToggle.isOn != twoJoysticksToggle;
+*/
+    }
+    
+}
