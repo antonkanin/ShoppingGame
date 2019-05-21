1ee543c2d45bd5e20caedaaff941badca8d04761
antonkanin <antony.kanin@gmail.com>
2019-05-21 17:40:14 +0300

    adding single joystick option

---
+++ Assets/Scripts/GUI/SingleJoystickUpdate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SingleJoystickUpdate : MonoBehaviour
+{
+    public Joystick Joystick;
+
+    public Vector2Value movement;
+    public Vector2Value rotation;
+
+    void Update()
+    {
+        movement.value.y = Joystick.Vertical;
+        rotation.value.x = Joystick.Horizontal;
+    }
+}
