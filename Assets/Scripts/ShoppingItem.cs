1ad2156ca04d4c9c45485fd460a250ebac10d7cb
antonkanin <antony.kanin@gmail.com>
2019-05-22 00:13:19 +0300

    adding item highlight - work in progress

---
+++ Assets/Scripts/ShoppingItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class ShoppingItem : MonoBehaviour
+{
+    public float distanceToPlayer = 1.0f;
+
+    public Transform playerPosition;
+
+    private void Start()
+    {
+        var player = GameObject.Find("Player");
+
+        if (player == null)
+        {
+            throw new NullReferenceException("Cannot find Player object on the scene");
+        }
+
+        playerPosition = player.transform;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(playerPosition.position, transform.position) <= distanceToPlayer)
+        {
+            SpotLightOn();  
+        }
+    }
+
+    void SpotLightOn()
+    {
+        
+        
+    }
+}
