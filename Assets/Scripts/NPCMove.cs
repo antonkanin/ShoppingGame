d75c14910c7d3dde471dea8bdc908dd5cbe9415e
antonkanin <antony.kanin@gmail.com>
2019-05-21 12:31:33 +0300

    playing with navmesh components

---
+++ Assets/Scripts/NPCMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private Transform destination;
+
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+        Vector3 targetVector = destination.transform.position;
+        navMeshAgent.SetDestination(targetVector);
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField]
+    private float debugRadius = 1.0f;
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, debugRadius);
+    }
+}
