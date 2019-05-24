e9c9815df74e503fb8a0a4a5bc12cc19802bf0f8
antonkanin <antony.kanin@gmail.com>
2019-05-25 00:05:48 +0300

    adding Left/Right buttons to control the player rotation

---
+++ Assets/Scripts/GUI/KeysUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeysUpdate : MonoBehaviour
+{
+    public InputController inputController;
+
+    // Start is called before the first frame update
+    public void ButtonLeftDown()
+    {
+        inputController.rotation.x = -1.0f;
+    }
+
+    public void ButtonRightDown()
+    {
+        inputController.rotation.x = 1.0f;
+    }
+
+    public void ButtonUp()
+    {
+        inputController.rotation.x = 0.0f;
+    }
+}
