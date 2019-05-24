3e819d30702e51b82c10065b26b2f4906e53931a
antonkanin <antony.kanin@gmail.com>
2019-05-24 23:42:54 +0300

    adding towers that shoot very round red bullets

---
+++ Assets/Scripts/Tower/Shooting.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform shootFromPos;
+
+    public float distanceToPlayer;
+
+    public GameObject bulletPrefab;
+
+    public float shootingInterval;
+
+    public float shootingForce;
+
+    public float bulletLifeTime;
+
+
+    private GameObject player;
+
+    private GameObject bulletsParent;
+
+    private float lastShotTime;
+
+    private bool isShotFired = false;
+
+    private Vector3 debug_bulletforce_direction;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+
+        if (player == null)
+        {
+            throw new NullReferenceException("Player game object not found");
+        }
+
+        bulletsParent = GameObject.Find("Bullets");
+
+        if (bulletsParent == null)
+        {
+            throw new NullReferenceException("Bullets game object not found");
+        }
+    }
+
+    void Update()
+    {
+        TryShootAtPlayer();
+    }
+
+    void TryShootAtPlayer()
+    {
+        var distance = (player.transform.position - shootFromPos.position).magnitude;
+
+        if (distance <= distanceToPlayer && !isShotFired)
+        {
+            StartCoroutine("Shoot");
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        isShotFired = true;
+        var bullet = Instantiate(bulletPrefab, shootFromPos.position, Quaternion.identity);
+        bullet.transform.parent = bulletsParent.transform;
+
+        bullet.transform.LookAt(player.transform.position);
+
+        bullet.GetComponent<Rigidbody>().AddForce(shootingForce * bullet.transform.forward,
+            ForceMode.Impulse);
+        
+        Destroy(bullet, bulletLifeTime);
+
+        yield return new WaitForSeconds(shootingInterval);
+
+        isShotFired = false;
+    }
+}
