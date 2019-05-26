19735032c76ec4df72de9259fc669deb956d49f1
antonkanin <antony.kanin@gmail.com>
2019-05-26 23:05:46 +0300

    #1 Шок от удара башней - done

---
+++ Assets/Scripts/Player/HitReaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HitReaction : MonoBehaviour
+{
+    public InputController input;
+
+    public float slowDownTime;
+
+    void OnCollisionEnter(Collision other)
+    {
+        const string BULLET_TAG = "Bullet";
+
+        if (other.gameObject.CompareTag(BULLET_TAG))
+        {
+            StartCoroutine("SlowDown");
+        }
+    }
+
+    IEnumerator SlowDown()
+    {
+        var originalSpeed = input.playerSpeed;
+        input.playerSpeed = 0;
+
+        yield return new WaitForSeconds(slowDownTime);
+
+        input.playerSpeed = originalSpeed;
+    }
+}
