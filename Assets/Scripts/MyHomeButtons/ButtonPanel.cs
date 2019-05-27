7b72c6c3564daf2af2617967e95f3aa4a155f5fb
SiarheiKrumkacheu <siarhei.krumkacheu@gmail.com>
2019-05-27 22:50:19 +0300

    Added scene MyHome

---
+++ Assets/Scripts/MyHomeButtons/ButtonPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject panel;
+    private bool objectActive = false;
+
+
+    public void showhidePannel()
+    {
+        if (objectActive == true) 
+        {
+            panel.gameObject.SetActive(false);
+            objectActive = false;
+        }
+        else if(objectActive == false)
+        {
+            panel.gameObject.SetActive(true);
+            objectActive = true;
+        }
+    }
+}
