c89ab43939289a5d1b915128e0195798e5f57d70
antonkanin <antony.kanin@gmail.com>
2019-05-28 23:55:59 +0300

    adding the button to switch to the Home scene

---
+++ Assets/Scripts/SO/SceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class SceneLoader : ScriptableObject
+{
+    // Start is called before the first frame update
+    public string shopScene;
+    public string homeScene;
+
+    void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadHome()
+    {
+        LoadScene(homeScene);
+    }
+
+    public void LoadShop()
+    {
+        LoadScene(shopScene);
+    }
+}
