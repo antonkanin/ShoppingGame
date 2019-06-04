ec281a25434b8159e63ef742f1f5c25b59fe9abc
Julia_Lemis <julialemis@mail.ru>
2019-06-04 15:38:25 +0300

    Добавила меню и скрипт

---
+++ Assets/Scripts/MainMenu/ControlSceneAndMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSceneAndMenu : MonoBehaviour
+{
+    public GameObject ButtonsMenu;
+    public GameObject Exitgame;
+    
+    public void ShowExitButtons()
+    {
+        ButtonsMenu.SetActive(false);
+        Exitgame.SetActive(true);
+    }
+
+    public void BackInMenu()
+    {
+        ButtonsMenu.SetActive(true);
+        Exitgame.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();   
+    }
+}
