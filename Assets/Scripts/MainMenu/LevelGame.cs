8e548169db03f926833e7cbee2f09f4546fbaa1f
Julia_Lemis <julialemis@mail.ru>
2019-06-04 17:13:55 +0300

    Связала сцены с кнопками в меню

---
+++ Assets/Scripts/MainMenu/LevelGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelGame : MonoBehaviour
+{
+   public void Game()
+    {
+        SceneManager.LoadScene ("ShopScene");
+    }
+}
