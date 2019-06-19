dd8a19a79b5c41d0825b935e37de0df6f883d040
SiarheiKrumkacheu <siarhei.krumkacheu@gmail.com>
2019-06-20 00:32:28 +0300

    Scene MyHome

--- Assets/Scripts/MyHomeButtons/ButtonsChoice.cs
+++ Assets/Scripts/MyHomeButtons/ButtonsChoice.cs
@@ -6,27 +6,35 @@ using UnityEngine.UI;
 public class ButtonsChoice : MonoBehaviour
 {
     [SerializeField] private Sprite spriteFurniture = null;
-    [SerializeField] private GameObject furniture = null;
+    [SerializeField] private GameObject imageFurniture = null;
     [SerializeField] private Text money = null;
     [SerializeField] private Text price = null;
     [SerializeField] private GameObject panelNoMany = null;
     [SerializeField] private int counter = 0;
     private float _couterMoney;
 
+    //[SerializeField] private GameObject panel = null;
+    //private bool objectActive = false;
+    //[SerializeField] private Sprite newImageFurniture = null;
+    //[SerializeField] private int newImageFurnit = 0;
+
     public void ChooseFurniture() 
-    {
+    {   
+        //newImageFurniture =  imageFurniture.GetComponent<Image>().sprite;
         ++counter;
-        furniture.GetComponent<Image>().sprite = spriteFurniture;
+        imageFurniture.GetComponent<Image>().sprite = spriteFurniture;
         if (counter % 2 == 0) 
         {    
             if (float.Parse(money.text) >= float.Parse(price.text))
             {
-                furniture.GetComponent<Image>().sprite = spriteFurniture;
+                imageFurniture.GetComponent<Image>().sprite = spriteFurniture;
                 _couterMoney = float.Parse(money.text) - float.Parse(price.text);
                 
                 price.text = "0";
 
                 money.text = _couterMoney.ToString();
+                //++newImageFurnit; 
+                
             }
             else
             {
@@ -42,4 +50,29 @@ public class ButtonsChoice : MonoBehaviour
         }
       
     }
+
+     /*public void showhidePannel()
+    {
+        if (newImageFurnit%2 == 0) print(" newImageFurnit false");
+        else if (newImageFurnit%2 != 0) print(" newImageFurnit true");
+
+        if (objectActive == true && newImageFurnit%2 != 0) 
+        {   
+            print("цвет изменен");
+            panel.gameObject.SetActive(false);
+            objectActive = false;
+        }
+        else if (objectActive == true && newImageFurnit%2 == 0)
+        {
+            //imageFurniture.GetComponent<Image>().sprite = newImageFurniture;
+            print("цвет не изменен");
+            panel.gameObject.SetActive(false);
+            objectActive = false;
+        }
+        else if (objectActive == false)
+        {
+            panel.gameObject.SetActive(true);
+            objectActive = true;
+        }
+    }*/
 }
